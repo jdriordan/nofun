cb80f5e44ca5e760a2aa0bc3134bb5ddc56543ab
Danny Riordan <dannyriordan@gmail.com>
2014-03-27 00:05:14 +0000

    added rudimentary backtracking

--- nofun.lua
+++ nofun.lua
@@ -1,8 +1,9 @@
 emu.softreset()
 emu.speedmode("maximum")
-states = {savestate.object(),savestate.object()}
+states = {savestate.object(1),savestate.object(2)}
 time = 0
 goal = 0
+chances = 10
 savetime = 100
 emu.message("HI")
 
@@ -26,6 +27,16 @@ emu.message("Saved initial state")
 xpos2=0
 xpos=0
 
+new=1
+old=2
+
+function swap()
+  temp=old
+  old=new
+  new=temp
+end
+
+
 while true do
   lives = memory.readbyte(0x075A)
   ypos  = 176 - memory.readbyte(0x00CE)
@@ -34,13 +45,14 @@ while true do
   xpos2 = memory.readbyte(0x0086)
   yvel  = memory.readbyte(0x009F)
   
-  xpos3=256*xpos+xpos2
+  xpos3=256*xpos+xpos2 -- this is the actual distance
 
   gui.text(50,50,"ypos: "..ypos)
   gui.text(50,60,"time: "..time)
-  gui.text(50,70,"xpos: "..xpos.."."..xpos2)
-  gui.text(50,80,"yards: "..xpos3)
+  gui.text(50,70,"goal: "..goal-xpos3)
+  gui.text(50,80,"chances: "..chances)
   gui.text(50,90,"yvel: "..yvel)
+  gui.text(50,100,"pipe: "..memory.readbyte(0x000E))
 
   if memory.readbyte(0x001D) == 0x03 
     then emu.message("flag") 
@@ -55,29 +67,35 @@ while true do
   end
 
 
-  if ypos<-50    or 
-     sprite==176 or
-     time > savetime 
+  if (ypos<-50    or 
+      sprite==176 or
+      time > savetime)
     then 
-         if time < -40 or goal-xpos>20
-           then savestate.load(states[2])
-                goal=goal-100
-                emu.message("loaded oldstate")
-           else savestate.load(states[1]) 
+         if chances==0 
+	   then --emu.pause()
+                savestate.load(states[old])
+		goal=xpos3+100
+		time=0
+		emu.message("had "..chances..", loaded old")
+		chances=10
+	   else chances=chances-1
+                savestate.load(states[new])
+                goal=xpos3+100
                 emu.message("Loaded newstate")
+                time=0
          end
-         time=-50
   end
 
   
   if time > (savetime - 10) 
-     and yvel==0
+  --   and yvel==0
      and xpos3>=goal
-    then savestate.save(states[1])
-         if ypos==0 then savestate.save(states[2]) end
+    then savestate.save(states[new])
+         swap()
          time=0
+         chances=10
          emu.message("Saving state")
-         goal=xpos3+150
+         goal=xpos3+100
   end
   
   joypad.set(1,{B=1,A=r(0.1),right=r(0.15)});
