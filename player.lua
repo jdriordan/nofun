1d9225176949ebe58cbb83d55ac6e64bd4c63c11
Danny Riordan <dannyriordan@gmail.com>
2014-08-05 20:21:45 +0100

    fixing silly mistakes

--- player.lua
+++ player.lua
@@ -4,7 +4,7 @@ for i=1,300,1 do emu.frameadvance() end
 function string2table(s)
 	t={}
 	str=s
-	str:gsub(".",function(c) table.insert(t,c) end)
+	str:gsub(".",function(c) table.insert(t,c==1) end)
 	return table
 end
 
@@ -15,7 +15,9 @@ function main()
 	os.execute("./simplify.sh temp_frame.png do_frame.png")
 	emu.message("simplified the image!")
 	bot = io.popen("./do.py")
-	cmd = string2table(bot:read(8))
+	str = bot:read(8)
+	emu.message(str)
+	cmd = string2table(str)
 	joypad.set(1,cmd)
 	emu.frameadvance()
 	bot:close()
