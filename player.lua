a7656de4e3f12f2f57457f07f473283dcf982796
Danny Riordan <dannyriordan@gmail.com>
2014-08-12 15:23:48 +0100

    it's alive!

--- player.lua
+++ player.lua
@@ -1,24 +1,40 @@
 --emu.softreset()
-for i=1,300,1 do emu.frameadvance() end
+--for i=1,300,1 do emu.frameadvance() end
+emu.message("hi")
 
 function string2table(s)
-	t={}
-	str=s
-	str:gsub(".",function(c) table.insert(t,c==1) end)
-	return table
+	emu.message("requested "..s)
+	buttons = joypad.get(1)
+	--buttons={up, down, left, right, A, B, start, select}
+	i=1
+	for key,val in pairs(buttons) do
+		buttons[key]=(string.sub(s,i,i)=='1')
+		i=i+1
+		--print("pressed ",key,buttons[key])
+	end
+	return buttons
+end
+
+function table2string(t)
+	s=""
+	for i,x in pairs(t) do 
+		s=s..(x and 1 or 0) end
+  	return s
 end
 
 
 function main()
 	gui.savescreenshotas("temp_frame.png")
-	emu.message("hi")
+	
 	os.execute("./simplify.sh temp_frame.png do_frame.png")
-	emu.message("simplified the image!")
+	--emu.message("simplified the image!")
 	bot = io.popen("./do.py")
 	str = bot:read(8)
-	emu.message(str)
 	cmd = string2table(str)
 	joypad.set(1,cmd)
+--[[	for k,v in pairs(cmd) do
+		print(k,v)
+	end]]
 	emu.frameadvance()
 	bot:close()
 end
